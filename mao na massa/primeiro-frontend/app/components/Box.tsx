f8fd1d391bb8d2fd3ab2fe2d0abd3ae70dad380e
evanzs <evandrofidk@hotmail.com>
2025-12-09 19:13:59 -0300

    feat(box.tsx) criando componente box para teste de uso

---
+++ mao na massa/primeiro-frontend/app/components/Box.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { Card } from 'react-bootstrap'
+
+const Box = () => {
+    const nome = "Teste Nome"
+
+  return (
+
+    <>
+      <Card>
+        <Card.Body>Primeira pagina</Card.Body>
+      </Card>  
+       
+    </>
+
+
+  )
+}
+
+export default Box
