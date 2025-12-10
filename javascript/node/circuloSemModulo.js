867b8eb3dbab63a30c00d58577a61a0feecbeb88
evanzs <evandrofidk@hotmail.com>
2025-12-10 16:42:27 -0300

    feat(circuloSemModulo.js): add classe com padrão antigo de modulos

---
+++ javascript/node/circuloSemModulo.js
@@ -0,0 +1,11 @@
+const {PI} = Math
+
+module.exports = class Circulo {
+
+    constructor(raio){
+        this.raio = raio
+    }
+
+    area = () => PI * (this.raio**2)
+    perimetro = () => 2 * PI * this.raio
+}
