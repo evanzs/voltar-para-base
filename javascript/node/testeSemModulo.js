bd914aa8ee34458378b41cc0f321860ea469052c
evanzs <evandrofidk@hotmail.com>
2025-12-10 16:43:23 -0300

    feat(testeSemModulo): add formas diferentes de importação de modulo

---
+++ javascript/node/testeSemModulo.js
@@ -0,0 +1,14 @@
+const circulo = require('./circuloSemModulo.js')
+const CirculoModulo = require('./circulo.js')
+
+
+const c1 = new circulo(1);
+
+console.log(c1.area());
+console.log(c1.perimetro());
+
+
+// aqui para baixo é pra dar erro
+
+const c2 = new CirculoModulo(1);
+console.log(c2.perimetro());
