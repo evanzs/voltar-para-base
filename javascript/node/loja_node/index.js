1a15794338d4a2cd8bf64833aefa362b6713672e
evanzs <evandrofidk@hotmail.com>
2025-12-16 14:05:26 -0300

    chore(index.js): add setup do swagger e documento

--- javascript/node/loja_node/index.js
+++ javascript/node/loja_node/index.js
@@ -2,19 +2,29 @@ import express from 'express'
 import produtoController from './controllers/produto-controller.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
+import swaggerUi from 'swagger-ui-express'
+import swaggerDoc from './swaggerDocument.json' assert {type:'json'}
 
 const app = express();
 
 const port = 3000
 
 
-app.use(bodyParser.json());
 
+//CONFIGS:
+app.use(bodyParser.json());
 app.use(cors({
     origin: '*',                 // ou domínio específico
     methods: ['GET','POST', 'PUT', 'DELETE'], // POST NÃO permitido
 }));
+
+
+
+
+//ROTAS:
+app.use(
+   '/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDoc)
+)
 
 
 app.use('/produtos', produtoController)// define a roda
