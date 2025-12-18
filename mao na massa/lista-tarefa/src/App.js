cfbb7a28cd65d044d6f99a9808f94f5b8744d50d
evanzs <evandrofidk@hotmail.com>
2025-12-18 17:17:06 -0300

    style(app.js): add estilo aos ocmponentes

--- mao na massa/lista-tarefa/src/App.js
+++ mao na massa/lista-tarefa/src/App.js
@@ -4,41 +4,43 @@ import './App.css';
 
 function App() {
   const [tarefas, setTarefas] = useState([
-    { id: 1, nome: "tarefa 1", completo: false },
-    { id: 2, nome: "tarefa 2", completo: false },
-    { id: 3, nome: "tarefa 3", completo: true },
+    { id: 1, titulo: "tarefa 1", completo: false },
+    { id: 2, titulo: "tarefa 2", completo: false },
+    { id: 3, titulo: "tarefa 3", completo: true },
   ])
 
   const [inputTarefa, setInputTarefa] = useState('')
 
   const addNovaTarefa = () => {
-    if(!inputTarefa) return;
+    if (!inputTarefa) return;
     const index = tarefas.length + 1;
-    const tarefa = { id: index, nome: inputTarefa, completo: false }
+    const tarefa = { id: index, titulo: inputTarefa, completo: false }
     setTarefas([...tarefas, tarefa])
     setInputTarefa('')
   }
   const concluirTarefa = (id) => {
-    tarefas[id-1].completo=true;
+    tarefas[id - 1].completo = true;
     setTarefas([...tarefas])
   }
 
   return (
-    <div className="App">
-      <div>
+    <div className="App container-principal">
+      <div className="input-nova-tarefa">
         {inputTarefa}
         <p>Nova Tarefa</p>
         <input type="text" value={inputTarefa} onChange={(e) => setInputTarefa(e.target.value)}></input>
         <button onClick={addNovaTarefa}>Add Tarefa</button>
       </div>
-      <div>
+      <div className="container-lista">
         <h3>Lista de tarefas pendente:</h3>
         {tarefas.map((tarefa) => (
           (!tarefa.completo &&
-            <div key={tarefa.id}>
-              <p>{tarefa.id} - {tarefa.nome}</p>
+            <div key={tarefa.id} className="container-tarefa">
+              <div className="container-titulo">{tarefa.id} - {tarefa.titulo}</div>
               {/* chama só com o click */}
-              <button onClick={ () => concluirTarefa(tarefa.id)}>Finalizar</button>
+              <div className="container-btn">
+                <button onClick={() => concluirTarefa(tarefa.id)}>Finalizar</button>
+              </div>
               {/* assim chama o click logo que a tela rendereizar:
               <button onClick={concluirTarefa(tarefa.id)}>Finalizar</button> */}
             </div>
@@ -47,12 +49,21 @@ function App() {
         ))}
       </div>
 
-      <div>
+      <div className="container-lista">
         <h3>Lista de tarefas finalizadas:</h3>
         {tarefas.map((tarefa) => (
-          (tarefa.completo && <p key={tarefa.id}>{tarefa.id} - {tarefa.nome}</p>)
+          (tarefa.completo &&
+          <div className="container-tarefa-finalizada">
+
+            
+            <div key={tarefa.id} className="container-finalizado">
+              {tarefa.id} - {tarefa.titulo}
+            </div>
+          </div>
+        )
         ))}
       </div>
+
 
     </div>
   );
