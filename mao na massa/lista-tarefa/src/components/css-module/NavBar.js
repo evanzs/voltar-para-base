32266e004a0ea0596bedf9976c6ce356bfb04bdd
evanzs <evandrofidk@hotmail.com>
2025-12-22 10:46:00 -0300

    feat(NavBar.js): criando arquivo js para trabalhar com style em modulo

---
+++ mao na massa/lista-tarefa/src/components/css-module/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './NavBar.module.css'
+
+export default function navBar() {
+  return (
+    <div className={styles.container}>
+      <h2>NavBar utilizando estilo com CSS MODULE</h2>
+    </div>
+  )
+}
