8a0b1a2c822023e1072d9b2ca0905878b00fd956
evanzs <evandrofidk@hotmail.com>
2025-12-22 10:46:50 -0300

    feat(app.js): incluindo style module e styled components

--- mao na massa/lista-tarefa/src/App.js
+++ mao na massa/lista-tarefa/src/App.js
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import './App.css';
 import InputTarefa from './components/InputTarefa';
 import CardTarefa from './components/CardTarefa';
-
+import NavBar from './components/css-module/NavBar'
+import BotaoCustom from './components/css-styled-components/BotaoCustom';
+import styled from 'styled-components'
 
 function App() {
   const [tarefas, setTarefas] = useState([
@@ -13,14 +15,17 @@ function App() {
 
   const [inputTarefa, setInputTarefa] = useState('')
 
+  const BotaoCustom = styled.button`background-color:red; color:white; padding:10px;`;
 
 
   return (
+    
     <div className="App container-principal">
+      <NavBar></NavBar>
       <InputTarefa inputTarefa={inputTarefa} setInputTarefa={setInputTarefa} tarefas={tarefas} setTarefas={setTarefas} ></InputTarefa>
       <CardTarefa titulo="Tarefas pendentes" tarefas={tarefas.filter(tarefa => !tarefa.completo)} setTarefas={setTarefas}/>
       <CardTarefa titulo="Tarefas Finalizadas" tarefas={tarefas.filter(tarefa => tarefa.completo)} setTarefas={setTarefas}/>
-
+      <BotaoCustom>Teste de Styled components</BotaoCustom>
       {/* <div className="container-lista">
         <h3>Lista de tarefas finalizadas:</h3>
         {tarefas.map((tarefa) => (
