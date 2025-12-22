3473cd352e2bc8e119b48dc7c1f2be2486c5cd01
evanzs <evandrofidk@hotmail.com>
2025-12-22 10:45:20 -0300

    feat(botaoCustom): criando componente botao para usar styled-components

---
+++ mao na massa/lista-tarefa/src/components/css-styled-components/BotaoCustom.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default function BotaoCustom() {
+    const BotaoCustom = styled.button`background-color:red`;
+
+    return (
+        <div>
+            <button>Testes</button>
+        </div>
+    )
+}
