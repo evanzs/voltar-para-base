f8a19466a9cdc7d11315c5f9dff15aa30e257e09
evanzs <evandrofidk@hotmail.com>
2025-12-23 12:57:18 -0300

    fix(tarefa.js): corrigindo erro logico para concluir tarefa

--- mao na massa/lista-tarefa/src/components/Tarefa.js
+++ mao na massa/lista-tarefa/src/components/Tarefa.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import './Tarefa.css'
-export default function Tarefa({tarefa,concluirTarefa}) {
+export default function Tarefa({ tarefa,tarefas, setTarefas }) {
+
+    const concluirTarefa = (id) => {
+        const index = tarefas.findIndex(tarefa => tarefa.id === id);
+        tarefas[index].completo = true;
+        setTarefas([...tarefas])
+
+    }
+
     return (
         <div key={tarefa.id} className="container-tarefa">
-            <div className={tarefa.completo? 'container-titulo-finalizado'  : 'container-titulo' }>{tarefa.id} - {tarefa.titulo}</div>
+            <div className={tarefa.completo ? 'container-titulo-finalizado' : 'container-titulo'}>{tarefa.id} - {tarefa.titulo}</div>
             {/* chama só com o click */}
-            {tarefa.completo ? 
-            <i className="fa-solid fa-check"></i>
-            :  
-            <div className="container-btn">
-                <button onClick={() => concluirTarefa(tarefa.id)}>Finalizar</button>
-            </div>}
+            {tarefa.completo ?
+                <i className="fa-solid fa-check"></i>
+                :
+                <div className="container-btn">
+                    <button onClick={() => concluirTarefa(tarefa.id)}>Finalizar</button>
+                </div>}
             {/* assim chama o click logo que a tela rendereizar:
               <button onClick={concluirTarefa(tarefa.id)}>Finalizar</button> */}
         </div>
