a77444c48eb357c4ea92e795c3014f0ff4dde243
evanzs <evandrofidk@hotmail.com>
2025-12-27 22:44:10 -0300

    feat(basics): incluindo teste basico com typescript e variaveis

---
+++ typescript/basics.ts
@@ -0,0 +1,6 @@
+let urserAge = 38;
+
+// não é recomendado fazer explicitar denotações de tipo desncesária uma vez que o valor já ta definido. 
+let userIdade: number = 40;
+// codigo com erro:
+// userIdade = '41';
